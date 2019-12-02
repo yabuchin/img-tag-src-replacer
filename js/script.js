96c9331096e761806b34d6f7ff060a5c43801289
yuichi.yabu <yuichi.yabu@yabook.local>
2019-12-02 09:45:41 +0900

    書換処理完成版

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const beforeImgSrc = './img/before.png';
 const afterImgSrc = './img/after.png';
-/*
+
+const animationName = `fadein_${Math.random().toString().substring(2)}`;
+
 const keyFrameCSS = ` 
-    @keyframes fadein {
+    @keyframes ${animationName} {
       from {
         opacity:0;
       }
@@ -13,7 +15,7 @@ const keyFrameCSS = `
 
 const animationCSS = `
     img[src="${beforeImgSrc}"] {
-      animation: fadein .5s step-end;
+      animation: ${animationName} .5s step-end;
     }`;
 
 document.styleSheets.item(0).insertRule(keyFrameCSS, 0);
@@ -21,16 +23,18 @@ document.styleSheets.item(0).insertRule(animationCSS, 0);
 
 document.addEventListener('DOMContentLoaded', () => {
   setTimeout(() => {
-    const beforeImg = document.querySelector(`img[src="${beforeImgSrc}"]`);
+    const beforeImg =
+      document.querySelector(`img[src="${beforeImgSrc}"]`);
     beforeImg.setAttribute('src', afterImgSrc);
-    document.styleSheets.item(0).deleteRule(0);
-    document.styleSheets.item(0).deleteRule(0);
-  }, 50);
-});
-*/
-document.addEventListener('DOMContentLoaded', () => {
-  setTimeout(() => {
-    const beforeImg = document.querySelector(`img[src="${beforeImgSrc}"]`);
-    beforeImg.setAttribute('src', afterImgSrc);
-  }, 50);
+
+    // 書き換え後にCSSルールの削除を行う
+    // 非同期で別のjsからルールが挿入されることを想定して、ruleのcssTextを検索して削除する
+    const styleSheets = document.styleSheets.item(0);
+    for (let i = styleSheets.rules.length-1; i >= 0; i--) {
+      const rule = styleSheets.rules[i];
+      if (rule.cssText.indexOf(animationName) >= 0) {
+        styleSheets.deleteRule(i);
+      }
+    }
+  }, 10);
 });
